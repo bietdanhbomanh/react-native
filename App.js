ec5f0d31bde7489702b19e383277d2885f89ae13
Pham Tai <bietdanhbomanh@gmail.com>
2022-06-13 00:15:55 +0700

    source init

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { FlatList, StyleSheet, Text, Image, View, ScrollView, Dimensions } from 'react-native';
+import { useLayoutEffect, useState } from 'react/cjs/react.development';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const win = Dimensions.get('window');
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        paddingTop: 22,
+    },
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 44,
+    },
+    img: {
+        flex: 1,
+        alignSelf: 'stretch',
+        width: win.width,
+    },
 });
+
+const logo = {
+    uri: 'https://cpad.ask.fm/450/774/576/-29996968-1tfd7tc-gpggmmc5d0og3a0/original/image.jpg',
+    width: win.width,
+    height: win.width,
+};
+
+const FlatListBasics = () => {
+    const [data, setData] = useState([]);
+    useLayoutEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((response) => response.json())
+            .then((data) => {
+                const demo = data.map((item) => {
+                    return { key: item.name };
+                });
+                setData([...demo, ...demo]);
+            });
+    }, []);
+    return (
+        <ScrollView>
+            <View style={styles.container}>
+                <Image source={logo} />
+                <FlatList
+                    keyExtractor={(item, index) => index}
+                    data={data}
+                    renderItem={({ item }) => <Text style={styles.item}>{item.key}</Text>}
+                />
+            </View>
+        </ScrollView>
+    );
+};
+
+export default FlatListBasics;
